00042702b630a5e03bfc10b09d9904f4b821aaed
Nakhun Song <nak4627@gmail.com>
2020-06-28 17:16:49 +0900

    implement: closing menu when clicked outside and key pressed with Escape

--- src/index.tsx
+++ src/index.tsx
@@ -24,13 +24,53 @@ interface IState {
 }
 
 class AnimationMenu extends React.PureComponent<IProps, IState> {
+  private menuRef: React.RefObject<HTMLInputElement>;
   constructor(props: any) {
     super(props);
     this.state = {
       open: false,
       color: props.color || 'black',
     }
+    this.menuRef = React.createRef();
+    this.closeMenu = this.closeMenu.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.handleKeyPressEscape = this.handleKeyPressEscape.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener(
+      'click',
+      this.handleClickOutside,
+      true,
+    );
+    document.addEventListener(
+      'keydown',
+      this.handleKeyPressEscape,
+      true,
+    );
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener(
+      'click',
+      this.handleClickOutside,
+      true,
+    );
+    document.removeEventListener(
+      'keydown',
+      this.handleKeyPressEscape,
+      true,
+    );
+  }
+
+  closeMenu() {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        open: false,
+      }
+    });
   }
 
   handleClick() {
@@ -43,11 +83,25 @@ class AnimationMenu extends React.PureComponent<IProps, IState> {
     });
     onClick();
   }
+
+  handleClickOutside(e: any) {
+    if (this.menuRef.current
+      && !this.menuRef.current.contains(e.target)) {
+        this.closeMenu();
+      }
+  }
+
+  handleKeyPressEscape(e: any) {
+    if (e.key === 'Escape') this.closeMenu();
+  }
   
   render() {
     const { open, color } = this.state
     return (
-      <Wrapper onClick={this.handleClick}>
+      <Wrapper
+        ref={this.menuRef}
+        onClick={this.handleClick}
+      >
         <Stick open={open} color={color} />
       </Wrapper>
     )
